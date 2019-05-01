a9f10f78500ee8ea9cdab17f700b95efb8f7995d
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2019-05-01 04:40:36 +0000

    Entrega

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
-    def show
+  def index
+    @articles = Article.all
+  end
+  def show
     @article = Article.find(params[:id])
   end
     def new
     end
+    
     def create
        @article = Article.new(article_params)
        @article.save
@@ -13,5 +17,5 @@ class ArticlesController < ApplicationController
     private 
     def article_params
         params.require(:article).permit(:title,:text)
-end
+    end
 end
